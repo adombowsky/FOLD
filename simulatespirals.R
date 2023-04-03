f8f19feb195b953df84199e4850768b272c7135f
Alex Dombowsky <alexdombowsky@Alexs-MacBook-Pro-3.local>
2023-04-03 11:33:48 -0400

    spirals simulations

---
+++ simulatespirals.R
@@ -0,0 +1,48 @@
+# packages
+library(grid)
+library(gridExtra)
+library(Rcpp)
+library(RcppArmadillo)
+library(foreach)
+library(doParallel)
+library(plyr)
+library(dplyr)
+library(reshape2)
+# this is the simulation study presented in the Supplementary Material
+# see the file "clusterspirals.R" in the "rfuncts" folder for one replication 
+
+# r functions
+source("rfuncts/mvnorm_gibbs.R")
+source("rfuncts/mnorm_D_apply.R")
+source("rfuncts/mergeloss.R")
+source("rfuncts/clusterspirals.R")
+source("rfuncts/foldball.R")
+sourceCpp("rcppfuncts/mnorm_D_arma.cpp")
+# parallel computing
+n.cores <- 10
+## create the cluster
+my.cluster <- parallel::makeCluster(
+  n.cores, 
+  type = "PSOCK"
+)
+## register it to be used by %dopar%
+doParallel::registerDoParallel(cl = my.cluster)
+# parallel loop
+N <- 300
+R <- 100
+x <- foreach(r = 1:R,
+             .packages = c("mcclust", "Rcpp", "cluster", "KODAMA", "mcclust.ext"),
+             .noexport = c("ldmvnorm_arma",
+                           "maketau",
+                           "makeSigma",
+                           "makeSigmacube",
+                           "makeSigmaDet",
+                           "mnorm_D_arma",
+                           "makeHellingerAvg")) %dopar% {
+                             clusterspirals(N=N)
+                           }
+sims <- matrix(unlist(x), ncol = 3, byrow=TRUE)
+sims_means <- colMeans(sims)
+round(sims_means,3)
+sims_sd <- apply(sims, 2, sd)
+round(sims_sd, 3)
