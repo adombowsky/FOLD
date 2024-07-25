9f7af3575f99668146423b7dd22377db0f4c6ace
adombowskye <alexdombowsky@Alexs-MacBook-Pro-6.local>
2024-07-25 15:54:50 -0400

    adding dependencies

--- dependencies.R
+++ dependencies.R
@@ -3,14 +3,11 @@ if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install(version = "3.19")
 # next, install main dependencies
-deps <- c("foreach", "doParallel", "dbscan", "ggplot2", "mclust","xtable", "plyr",
+install.packages(c("foreach", "doParallel", "dbscan", "ggplot2", "mclust","xtable", "plyr",
           "dplyr", "reshape2", "grid", "latex2exp", "gridExtra", "plyr", "dplyr",
-          #"scran", "scuttle", "M3Drop",
           "latex2exp", "cowplot", "uwot",
           "RColorBrewer", "RSSL", "mvtnorm",
-          "ggforce", "KODAMA", "devtools")
-install.packages(deps)
-rm(deps)
+          "ggforce", "KODAMA", "devtools"))
 # install Bioconductor packages
 BiocManager::install("scran")
 BiocManager::install("scuttle")
